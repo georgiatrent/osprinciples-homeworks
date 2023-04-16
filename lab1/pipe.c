2e0e6f6210710f64974f25482daa68d7c998a46a
georgiatrent <georgia@trentalange.net>
2023-04-16 16:39:59 -0700

    tiny piece 0: counting arguments

--- lab1/pipe.c
+++ lab1/pipe.c
@@ -1,4 +1,10 @@
 int main(int argc, char *argv[])
 {
+	printf("how many args are there: %d\n", argc);
+	printf("what are they:\n");
+	for (int i = 0; i < argc; i++)
+	{
+		printf("argv %d is: %s \n", i, argv[i]);
+	}
 	return 0;
 }
