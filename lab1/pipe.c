3ca4d660e34efe18e723c721b509a4befe6d7d80
georgiatrent <georgia@trentalange.net>
2023-04-19 00:37:04 -0700

    tiny piece 2: understanding child processes

--- lab1/pipe.c
+++ lab1/pipe.c
@@ -7,10 +7,22 @@
 
 int main(int argc, char *argv[])
 {
-	printf("I am a lazy program, letting other program do work.\n");
-	printf("I will just wait for their report\n\n");
-	execlp("ls", "ls", "-a", "-l", NULL);
-	printf("They finished; Done!\n");
-	return 0;
+  int return_code = fork();
+  if (return_code == 0) {
+    printf("This is the child process!\n");
+    execlp("ls", "ls", NULL);
+    exit(0);
+  } else if (return_code > 0) {
+    printf("I am lazy parent, letting my child to ls the directory\n");
+    printf("I will just wait for their report\n");
+    int pid = return_code;
+    int status = 0;
+    waitpid(pid, &status, 0);
+    printf("Child process exits with code: %d\n", WEXITSTATUS(status));
+  } else {
+    printf("Child process creation error! \n");
+  }
+  printf("They finished; Done!\n");
+  return 0;
 }
 
