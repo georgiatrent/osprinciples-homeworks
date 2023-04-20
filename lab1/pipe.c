8d5fc02fd93d24ffb6e54a41a15e57724aea449d
georgiatrent <georgia@trentalange.net>
2023-04-20 14:29:35 -0700

    chunk 1: execute input command

--- lab1/pipe.c
+++ lab1/pipe.c
@@ -5,17 +5,11 @@
 #include <sys/wait.h>
 #include <unistd.h>
 
-int main(void) {
-
-  int fd = open("my_new_file.txt", O_APPEND | O_WRONLY | O_CREAT); // opens a new file
-  // fd points to the txt file now
-  printf("%d", fd);
-  if (fd < 0)
-    printf("Error opening the file\n");
-
-  dup2(fd, STDOUT_FILENO); // now the STDOUT_FILENO points to "my_new_file.txt"
-  printf("This message will be printed to the my_new_file.txt, instead of the "
-         "console.\n");
+int main(int argc, char *argv[]) {
+  if(argv[1] != NULL){
+    char* argument = argv[1];
+    execlp(argument, argument, NULL);
+  }
 
   return 0;
 }
