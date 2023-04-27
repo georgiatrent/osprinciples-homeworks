dd343491a7b8370f43b60ade2dbd880d27901623
georgiatrent <georgia@trentalange.net>
2023-04-26 23:34:55 -0700

    final chunk: passes unit tests and checks errnos

--- lab1/README.md
+++ lab1/README.md
@@ -1,18 +1,22 @@
-## UID: 123456789
+## UID: XXX
 (IMPORTANT: Only replace the above numbers with your true UID, do not modify spacing and newlines, otherwise your tarfile might not be created correctly)
 
 ## Pipe Up
 
-One sentence description
+This program takes in a variable amount of arguments, taking the first argument following the executable and running it as if it were a shell command. The output of this command is then used as the input of the second argument. The last argument in the string outputs to the terminal once executed.
 
 ## Building
 
-Explain briefly how to build your program
+Build the program using: make
 
 ## Running
 
-Show an example run of your program, using at least two additional arguments, and what to expect
+Run the program using:
+./pipe arg1 arg2 arg3 ...
+Without any arguments following ./pipe, the program will exit.
+./pipe ls wc
+This will use the output of ls (which outputs a list of the contents in the current directory) as the input of wc (which counts the words in its input and outputs the number). Your output should be number of lines, words, and characters in ls's output.
 
 ## Cleaning up
 
-Explain briefly how to clean up all binary files
+Clean up the extra files by running: make clean
--- lab1/pipe.c
+++ lab1/pipe.c
@@ -7,38 +7,86 @@
 
 int main(int argc, char *argv[]) {
 
-  int to_shell[2];
-  pipe(to_shell);
-
-
-  int ret = fork();
-  if(ret == 0)
+  if(argc <= 1)
   {
-    char buffer[4096];
-    close(to_shell[1]);
-    close(0);
-    dup(to_shell[0]);
-    close(to_shell[0]);
-    // read(0, buffer, 300);
-    // printf(buffer);
-    execlp(argv[2], argv[2], NULL);
+    exit(EINVAL); //just ./pipe
   }
-  else if (ret > 0){
-    close(to_shell[0]);
-    close(1);
-    dup(to_shell[1]);
-    close(to_shell[1]);
-    execlp(argv[1], argv[1], NULL);
-    //write(1, "ls", 10);
-    int pid = ret;
-    int status = 0;
-    waitpid(pid, &status, 0);
-    //printf("hi baby");
+  else if(argc == 2)
+  {
+    if(execlp(argv[1], argv[1], NULL) == -1) //only two arguments, ./pipe and something else
+    {
+      exit(errno);
+    }
+  }
 
+  int ret = 0;
+  int fds[2];
+  int in = STDIN_FILENO;
+
+  for(int i = 1; i < argc; i++)
+  {
+    if (pipe(fds) == -1)
+    {
+      exit(errno);
+    }
+    ret = fork();
+    if(ret == 0)
+    {
+      //child process
+
+      if(close(fds[0])) //close read end
+      {
+        exit(errno);
+      }
+      if(dup2(in, STDIN_FILENO) == -1) //set in to input
+      {
+        exit(errno);
+      } //change where you get input
+      if(argv[i + 1] != NULL) //only change where output goes if
+      {//there's another argument, otherwise send to terminal
+        if(dup2(fds[1], STDOUT_FILENO) == -1) //change output to write end
+        {
+          exit(errno);
+        }
+      }
+      if(execlp(argv[i], argv[i], NULL) == -1)
+      {
+        exit(errno);
+
+      };
+    }
+    else if(ret > 0)
+    {
+      if(close(fds[1]))
+      {
+        exit(errno);
+      }
+      if(close(in))
+      {
+        exit(errno);
+      }
+      in = fds[0];
+
+      int pid = ret;
+      int status = 0;
+      waitpid(pid, &status, 0);
+
+      if(WIFEXITED(status))
+      {
+        int getOut = WEXITSTATUS(status);
+        if(getOut != 0)
+        {
+          exit(getOut);
+        }
+      }
+
+    }
+    else
+    {
+      exit(errno);
+    }
   }
-  else{
-    printf("Error forking");
-  }
+
   return 0;
 }
 
